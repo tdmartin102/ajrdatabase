e50bd50cd4d290282187bb21434523db0b748cd6
Tom Martin <tom.martin@riemer.com>
2012-01-17 16:20:59 -0500

    Use method swiveling instead of poseAs:

--- EOModeler/Fixes/NSTextFieldFix.m
+++ EOModeler/Fixes/NSTextFieldFix.m
@@ -8,17 +8,27 @@
 
 #import "NSTextFieldFix.h"
 
-@implementation NSTextFieldFix
+#import <objc/objc-class.h>
+
+@implementation NSTextField (EOModler)
 
 + (void)load
 {
-	[self poseAsClass:[NSTextField class]];
+	#warning We need to swizzel this class
+	Method		originalMethod;
+	Method		ourMethod;
+
+	//[self poseAsClass:[NSComboBoxCell class]];
+	
+	originalMethod = class_getInstanceMethod([NSTextField class], @selector(setStringValue:));
+	ourMethod = class_getInstanceMethod([NSTextField class], @selector(_ajrSetStringValue:));
+	method_exchangeImplementations(originalMethod, ourMethod);
 }
 
-- (void)setStringValue:(NSString *)aValue
+- (void)_ajrSetStringValue:(NSString *)aValue
 {
-	if (aValue == nil) [super setStringValue:@""];
-	else [super setStringValue:aValue];
+	if (aValue == nil) [self _ajrSetStringValue:@""];
+	else [self _ajrSetStringValue:aValue];
 }
 
 @end
