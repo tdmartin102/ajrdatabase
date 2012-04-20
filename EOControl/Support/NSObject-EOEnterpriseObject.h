a20ff160abab54e0be8c83881143794a3de9dd78
Tom Martin <tom.martin@riemer.com>
2012-04-20 15:19:38 -0400

    Added new method primitiveValueForKey: which behaves the same as the depreciated method storedValueForKey:  previously I felt this was not important as I felt the storedValueForKey: was being used for performance reasons.  I now understand that it was an attempt to bypass EO accessor logic.  logic which sometimes can cause faults to fire and also can sometimes affect the stored value.  This method could be optimized for speed.

--- EOControl/Support/NSObject-EOEnterpriseObject.h
+++ EOControl/Support/NSObject-EOEnterpriseObject.h
@@ -71,4 +71,15 @@
 // I have implemented setPrimitiveValue:forKey here to replace takeStoredValue:forKey:
 - (void)setPrimitiveValue:(id)value forKey:(NSString *)key;
 
+// tom.martin @ riemer.com 2012-04-19
+// calling storedValueForKey bypasses EO logic to access the same values set by
+// setPrimitiveValue:forKey:  This is what we want to do when we are working with
+// database and context snapshots.  It also will prevent an object from firing a 
+// fault for a relationship when there is EO code that would case that to happen
+// with the normal accessor methods.  To replace that depreciated method I have
+// supplied the following which will do the same thing as storedValueForKey.
+// not as fast I am sure, but at least it gets the job done.
+- (id)primitiveValueForKey:(NSString *)key;
+
+
 @end
